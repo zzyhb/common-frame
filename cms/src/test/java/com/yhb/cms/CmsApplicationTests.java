5c7b4724cfe8c2ab8fbd164622f8b85723575394
fusu <YiHaibo@ifugle.com>
2021-01-16 01:04:09 +0800

    add: commit

---
+++ cms/src/test/java/com/yhb/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhb.cms;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
